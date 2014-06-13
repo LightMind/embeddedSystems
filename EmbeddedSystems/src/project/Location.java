88a646b9a30d136f76c16369dd2c70060efb7b67
Lukas Walther <lukas100@cs.au.dk>
2014-06-13 12:52:42 +0200

    Added a class for graphs.

---
+++ EmbeddedSystems/src/project/Location.java
@@ -0,0 +1,53 @@
+package project;
+
+import java.util.List;
+
+import lejos.geom.Point;
+
+public class Location {
+	public Point position;
+	public final int id;
+	public List<Location> connections;
+	
+	public void connectTo(Location l){
+		if(!l.equals(this) && !connections.contains(l)){
+			connections.add(l);
+			l.connections.add(this);
+		}
+	}
+	
+	public void disconnect(Location l){
+		if(connections.contains(l)){
+			connections.remove(connections.indexOf(l));
+			l.connections.remove(l.connections.indexOf(this));
+		}
+	}	
+	
+	public Location(int identifier, Point pos){
+		position = pos;
+		id = identifier;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+}
